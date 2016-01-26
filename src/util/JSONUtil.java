674e3580e723e059abdc73e04d4a587ca147721d
xhuy0117 <chuy_punk@hotmail.com>
2016-01-26 15:11:25 -0600

    Changes in JSONUtil class for his use

--- src/util/JSONUtil.java
+++ src/util/JSONUtil.java
@@ -5,6 +5,6 @@ import org.json.JSONObject;
 public class JSONUtil {
 	public static String toJSON(Object object) {
 		JSONObject jsonObject = new JSONObject(object);
-		return jsonObject.toString();
+		return jsonObject.toString();	
 	}
 }
