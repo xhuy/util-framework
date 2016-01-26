e11d6304ad914eb6feeb8f2f2daf48cad6ad6276
xhuy0117 <chuy_punk@hotmail.com>
2016-01-26 15:07:02 -0600

    =Adding org.JSON library and JSONUtil class for his use

---
+++ src/util/JSONUtil.java
@@ -0,0 +1,10 @@
+package util;
+
+import org.json.JSONObject;
+
+public class JSONUtil {
+	public static String toJSON(Object object) {
+		JSONObject jsonObject = new JSONObject(object);
+		return jsonObject.toString();
+	}
+}
